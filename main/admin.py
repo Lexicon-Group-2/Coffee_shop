8b4859f02ecd71c0fe151d2a487c9038e676a501
bojan-vujic <bojan.vujic@yahoo.com>
2022-06-07 14:32:59 +0200

    Added template for our webshop.

---
+++ main/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from main.models import Profile
+
+
+admin.site.register(Profile)
+
---
+++ main/views.py
@@ -0,0 +1,85 @@
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from main.forms  import UserForm, UserProfileForm
+from main.models import Profile
+
+# an email should be unique, we will use this little fix
+User._meta.get_field('email')._unique = True
+
+
+def index(request):
+  return render(request, 'main/index.html', {})
+
+
+
+def signup(request):
+  registered = False
+
+  if request.method == 'POST':
+    user_form = UserForm(data = request.POST)
+    profile_form = UserProfileForm(data = request.POST)
+
+    if user_form.is_valid() and profile_form.is_valid():
+      user = user_form.save()
+      #auth_login(request, user) # this will automatically log in user after creating an account
+      user.set_password(user.password)
+      user.save()
+      
+      profile = profile_form.save(commit=False)
+      profile.user = user
+
+      if 'image' in request.FILES:
+        profile.image = request.FILES['image']
+        profile.save()
+        
+      registered = True
+    else:
+      print(user_form.errors, profile_form.errors)
+  else:
+    user_form = UserForm()
+    profile_form = UserProfileForm()
+
+  context = {
+    'user_form': user_form,
+    'profile_form': profile_form,
+    'registered': registered,
+  }
+
+  return render(request, 'main/signup.html', context)
+
+
+
+def user_login(request):
+  if request.method == 'POST':
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    
+    # use django authentication function
+    user = authenticate(username=username, password=password)
+    
+    # check if user passed the authentication process
+    if user:
+      if user.is_active:
+        login(request, user)
+        return HttpResponseRedirect(reverse('home'))
+      else:
+        return HttpResponse("Account not active")
+    else:
+      return HttpResponse("Invalid login details!")
+
+  return render(request, 'main/login.html', {})
+
+@login_required
+def user_logout(request):
+  logout(request)
+  return render(request, 'main/logout.html', {})
+
+
+def user(request):
+  return render(request, 'main/user.html', {})
+
