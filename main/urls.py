9ee6965a6c9df96b8e7eaf0b6aaf8ae012040eca
saramaher1 <sara.almaher94@gmail.com>
2022-06-10 12:26:08 +0200

    adding home page to the view.py and urls and start styling it

--- main/urls.py
+++ main/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.index, name='landing'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name="logout"),
     path('user/', views.user, name="user"),
+    path('home/',views.home,name='home')
+
 ]
