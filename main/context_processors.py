2520ba1234a92e93f94e3fce1fe6a9f3716654c5
bojan-vujic <bojan.vujic@yahoo.com>
2022-06-11 20:13:55 +0200

    Fixed context processor for non authenticated users.

--- main/context_processors.py
+++ main/context_processors.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from coffee_store.models import Product
 
 # This will be updated once we have our db
@@ -14,11 +15,15 @@ def arrow(request):
     arrow = "&#10140;"
     return {'arrow': arrow}
 
+
 def favorite_items(request):
-    products = Product.objects.filter(favourites=request.user)
-    favorite_items = len(products)
-    if favorite_items == 0:
-        image = "/media/wish_list_empty.png"
-    else:
-        image = "/media/wish_list.png"
+    favorite_items = 0
+    image = "/media/wish_list_empty.png"
+    if request.user.id:
+        products = Product.objects.filter(favourites=request.user)
+        favorite_items = len(products)
+        if favorite_items == 0:
+            image = "/media/wish_list_empty.png"
+        else:
+            image = "/media/wish_list.png"
     return {'favorite_items': favorite_items, 'favorite_items_image': image}
