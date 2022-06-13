ea41fb93f022060ffebd5ac92f02d6d929cb655e
saramaher1 <sara.almaher94@gmail.com>
2022-06-13 12:57:40 +0200

    update files in homepage

--- static/js/showPassword.js
+++ static/js/showPassword.js
@@ -1,32 +1,36 @@
+var togglePassword = document.querySelector("#togglePassword"),
+  password = document.querySelector("#id_password");
 
-var togglePassword = document.querySelector('#togglePassword'),
-    password = document.querySelector('#id_password')
-
-togglePassword.addEventListener('click', () => {
-  var type = password.getAttribute('type')
-  if (type === 'password') {
-    password.setAttribute('type', 'text')
-    togglePassword.classList.remove("fa-eye")
-    togglePassword.classList.add("fa-eye-slash")
+togglePassword.addEventListener("click", () => {
+  var type = password.getAttribute("type");
+  if (type === "password") {
+    password.setAttribute("type", "text");
+    togglePassword.classList.remove("fa-eye");
+    togglePassword.classList.add("fa-eye-slash");
   } else {
-    password.setAttribute('type', 'password')
-    togglePassword.classList.remove("fa-eye-slash")
-    togglePassword.classList.add("fa-eye")
+    password.setAttribute("type", "password");
+    togglePassword.classList.remove("fa-eye-slash");
+    togglePassword.classList.add("fa-eye");
   }
-})
+});
 
 function removeFromDOM(className = ".play-container") {
-  document.querySelectorAll('.' + className).forEach(el => el.remove());
+  document.querySelectorAll("." + className).forEach((el) => el.remove());
 }
 
-var closeFieldErrorBox = document.getElementsByClassName('btn-close')[0]
+var closeFieldErrorBox = document.getElementsByClassName("btn-close")[0];
 
-closeFieldErrorBox.addEventListener('click', () => {
-  removeFromDOM('alert-danger')
-})
+closeFieldErrorBox.addEventListener("click", () => {
+  removeFromDOM("alert-danger");
+});
 
-var fields = document.getElementsByClassName('is-invalid')
+var fields = document.getElementsByClassName("is-invalid");
 
-for (let field of fields){
-  console.log(field.value)
+for (let field of fields) {
+  console.log(field.value);
 }
+
+$("#bologna-list a").on("click", function (e) {
+  e.preventDefault();
+  $(this).tab("show");
+});
