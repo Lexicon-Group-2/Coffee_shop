a5cf8e536563f95fa912c9e958511b9b0a1b0bfd
bojan-vujic <bojan.vujic@yahoo.com>
2022-06-14 22:51:21 +0200

    Revert "Merge remote-tracking branch 'origin/Yuran'"
    This reverts commit 8cc569256dcfea72623f19aed58d87353da7f420, reversing
    changes made to 9fc57993a1cd50bdf3dbb2c3f33e63916c7cc39a.

--- wish_list/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WishListConfig(AppConfig):
-    name = 'wish_list'
--- wish_list/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('wish_list/', views.wish_list, name="wish_list"),
-]
